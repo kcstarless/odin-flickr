77886cb90e0f2ab9a0bd57c64d0b2a855a6c112b
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-06-11 07:08:57 +1000

    fully working application, able search Flickr user_id to retrieve user's public photos

---
+++ README.md
@@ -0,0 +1,5 @@
+# [The Odin Project: Flickr-API](https://www.theodinproject.com/lessons/ruby-on-rails-flickr-api)
+
+## Description
+This project is to build simple Flickr-API using full-featured client gem called Flickr 2.0.
+Also implemented on this project is the use of Figaro gem to masquerade the api and shared keys. 
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,24 @@
+class StaticPagesController < ApplicationController
+  before_action :flickr_initilization
+
+  def index
+    flickr_id = params[:flickr_id]
+
+    begin
+      if flickr_id.present?
+        @photos = @flickr.photos.search user_id: flickr_id
+      else
+        @photos = @flickr.photos.getRecent
+      end
+    rescue Flickr::FailedResponse => e
+      @error = "Error with connection to Flickr API: #{e.message}"
+      @photos = []
+    end
+  end
+
+  private
+
+  def flickr_initilization
+    @flickr = Flickr.new Figaro.env.flickr_api_key, Figaro.env.flickr_shared_secret
+  end
+end
